bb3bf7236d5a6595532caed2ceecdd08a95503c6
utbrott <rosinskimk@gmail.com>
2023-01-25 19:25:48 +0100

    feat: main program: perodic data exchange

--- stm32-lorawan-pio/src/main.cpp
+++ stm32-lorawan-pio/src/main.cpp
@@ -1,11 +1,59 @@
 #include "main.h"
 
+/**
+ * Defines how the module behaves
+ * Options: MASTER, SLAVE
+ */
+#define MODULE_TYPE LoRa::SLAVE
+
+uint8_t loraMessage[8];
+uint8_t receivedMessage[8];
+
+bool nextMessage = true;
+uint32_t currentTime = 0;
+
+LoRa::ModuleType_t moduleType = MODULE_TYPE;
+DataRead_t readData;
+DataReceived_t receivedData;
+
 void setup()
 {
-  // put your setup code here, to run once:
+  /* Init LoRa shield */
+  LoRa::ShieldInit(MODULE_TYPE);
+
+  /* Init data container structs */
+  BME280::DataInit(&readData);
+  LoRa::DataInit(&receivedData);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  switch (moduleType)
+  {
+  case LoRa::SLAVE:
+    if (nextMessage)
+    {
+      BME280::ReadData(&readData);
+      LoRa::SendMessage(&readData, loraMessage);
+
+      currentTime = millis();
+      nextMessage = false;
+    }
+
+    if (millis() - currentTime >= SEND_PERIOD_MS)
+    {
+      nextMessage = true;
+    }
+    break;
+
+  case LoRa::MASTER:
+    if (loraRadio.read(receivedMessage) > 0)
+    {
+      LoRa::ReceiveMessage(&receivedData, receivedMessage);
+    }
+    break;
+
+  default:
+    break;
+  }
 }
--- stm32-lorawan-pio/src/main.h
+++ stm32-lorawan-pio/src/main.h
@@ -5,4 +5,14 @@
 #include "lora.h"
 #include "bme280_sensor.h"
 
+extern DataRead_t readData;
+extern DataReceived_t receivedData;
+extern LoRa::ModuleType_t moduleType;
+
+extern uint8_t loraMessage[8];
+extern uint8_t receivedMessage[8];
+
+extern bool nextMessage;
+extern uint32_t currentTime;
+
 #endif /* _MAIN_H */
