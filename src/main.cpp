aa84d3aeef03e8dc5e554dc39f33808e8982f3b5
utbrott <rosinskimk@gmail.com>
2023-01-25 22:56:35 +0100

    refactor(main): remove unused variables

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,15 @@
  * Defines how the module behaves
  * Options: MASTER, SLAVE
  */
-#define MODULE_TYPE LoRa::SLAVE
+#define MODULE_TYPE LoRa::MASTER
 
-uint8_t loraMessage[8];
 uint8_t receivedMessage[8];
 
 bool nextMessage = true;
 uint32_t currentTime = 0;
 
 LoRa::ModuleType_t moduleType = MODULE_TYPE;
-DataRead_t readData;
+DataRead_t sensorData;
 DataReceived_t receivedData;
 
 void setup()
@@ -22,24 +21,24 @@ void setup()
   LoRa::ShieldInit(MODULE_TYPE);
 
   /* Init data container structs */
-  BME280::DataInit(&readData);
+  BME280::DataInit(&sensorData);
   LoRa::DataInit(&receivedData);
 }
 
 void loop()
 {
+
   switch (moduleType)
   {
   case LoRa::SLAVE:
     if (nextMessage)
     {
-      BME280::ReadData(&readData);
-      LoRa::SendMessage(&readData, loraMessage);
+      BME280::ReadData(&sensorData);
+      LoRa::SendMessage(&sensorData);
 
       currentTime = millis();
       nextMessage = false;
     }
-
     if (millis() - currentTime >= SEND_PERIOD_MS)
     {
       nextMessage = true;
@@ -49,6 +48,7 @@ void loop()
   case LoRa::MASTER:
     if (loraRadio.read(receivedMessage) > 0)
     {
+
       LoRa::ReceiveMessage(&receivedData, receivedMessage);
     }
     break;
@@ -56,4 +56,3 @@ void loop()
   default:
     break;
   }
-}
